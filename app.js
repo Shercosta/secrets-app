bc37d81aa1c443d923bc008e386156a891903f2c
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-10-19 01:00:46 +0700

    login & register to view secrets
    currently you can only access "secrets" by routing to login or register. I want for /secrets to see if we are logged in, if not, redirected to login / register.

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -17,8 +27,24 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await User.findOne({ email: username }).exec(res.render("secrets"));
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save().then(res.render("secrets"));
 });
 
 app.listen(3000, () => {
